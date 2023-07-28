c1e20ab1507a06fc7bc2bdb443361d21514dc509
Arnav <arnavkul07@gmail.com>
2023-07-28 18:43:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function login(){
+
+    let form = document.getElementById('form')
+
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        let username = document.getElementById('username').value
+        let password = document.getElementById('password').value
+
+        //saving values locally
+
+        localStorage.setItem('username',username)
+        localStorage.setItem('password',password)
+
+        alert("Your data is stored in local storage")
+    })
+}
